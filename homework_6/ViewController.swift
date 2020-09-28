9470e5b25ccaa80f4f421c316066233ee1012699
Iskander Nizamov <nizamov-iskander@yandex.ru>
2020-09-28 15:30:20 +0300

    Initial Commit

---
+++ homework_6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework_6
+//
+//  Created by macbook on 28.09.2020.
+//  Copyright © 2020 Iskander Nizamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
