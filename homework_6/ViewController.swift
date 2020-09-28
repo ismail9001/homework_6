967b7ff658fb5230630a0b5f56882de3ac291a72
Iskander Nizamov <nizamov-iskander@yandex.ru>
2020-09-28 15:55:52 +0300

    homework6

--- homework_6/ViewController.swift
+++ homework_6/ViewController.swift
@@ -8,6 +8,47 @@
 
 import UIKit
 
+struct Queue<T: BinaryInteger> {
+    var list = [T]()
+    
+    mutating func filter(_ even: Bool ) -> [T] {
+        var count = list.count
+        if even == true {
+            for index in list.reversed() {
+                count -= 1
+                if Int(index) % 2 == 0 {
+                    list.remove(at: count)
+                }
+            }
+        } else {
+            for index in list.reversed() {
+                count -= 1
+                if Int(index) % 2 != 0 {
+                    list.remove(at: count)
+                }
+            }
+        }
+        return list
+    }
+    
+    mutating func map(_ value: Int)-> [T] {
+        var newArr: [T] = []
+        for index in list {
+            newArr.append(index + T(value))
+        }
+        return newArr
+        
+    }
+    
+    subscript(index: Int) -> T? {
+        if index >= list.count {
+            return nil
+        } else {
+            return list[index]
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
